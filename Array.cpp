42cad482c0ce04d05cf5548602a0c62b66795b49
Prithwi Kumar <prithwikumar871@gmail.com>
2025-01-14 15:02:27 +0530

    Array (Traversing)

---
+++ Array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// ================>>>  Traversing  <<<====================
+
+int main(){
+    int arr[5] = {10, 0, 20, 0, 30};
+    for(int i=0; i<5; i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
+
+
+
