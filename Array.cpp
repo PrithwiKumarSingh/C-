09398907e6b165db2f57f4342ea35e951e76ae85
Prithwi Kumar <prithwikumar871@gmail.com>
2025-01-14 15:52:01 +0530

    Array (Deletion)

--- Array.cpp
+++ Array.cpp
@@ -15,24 +15,48 @@ using namespace std;
 //===============>> Insertion <<==================
 
 
-int main() {
-    int arr[50] = {10, 30, 40, 50, 60};
-    int num, pos;
-    cout<<"Enter num and Pos : ";
-    cin>>num>>pos;
+// int main() {
+//     int arr[50] = {10, 30, 40, 50, 60};
+//     int num, pos;
+//     cout<<"Enter num and Pos : ";
+//     cin>>num>>pos;
 
-    // Shift elements to the right to make space for the new element
-    for (int i = 5; i > pos; i--) {
-        arr[i] = arr[i - 1];
+//     // Shift elements to the right to make space for the new element
+//     for (int i = 5; i > pos; i--) {
+//         arr[i] = arr[i - 1];
+//     }
+
+//     // Insert the new element at the specified position
+//     arr[pos] = num;
+
+//     cout << "The New Array is:" << endl;
+//     for (int i = 0; i < 6; i++) {
+//         cout << "index " << i << " : "<< arr[i] << endl;
+//     }
+
+//     return 0;
+// }
+
+//===============>>  Deletion <<======================
+
+int main(){
+    int arr[5] = {3, 4, 6, 8, 9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int elem;
+    int pos = 0;
+    cout<<"Enter your Elem : ";
+    cin>>elem;
+    for(int i=0; i<n; i++){
+        if(elem == arr[i]){
+            pos = i;
+        }
     }
-
-    // Insert the new element at the specified position
-    arr[pos] = num;
-
-    cout << "The New Array is:" << endl;
-    for (int i = 0; i < 6; i++) {
-        cout << "index " << i << " : "<< arr[i] << endl;
+    for(int i=pos; i<5; i++){
+        arr[i] = arr[i+1];
     }
-
+    for(int i=0; i<4; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
